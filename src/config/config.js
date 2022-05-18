e6d7156ca8050e5fcc75c92d6640505a6cdde055
Chipsa12 <ivango23456@gmail.com>
2022-05-19 00:15:56 +0400

    Updates

--- src/config/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = `http://localhost:5000/`
-export const API_URL_STATIC = `http://45.90.35.187:5000/static/`
-export const API_WEATHER_BIT = '36409b78283a4712b2a7a0516373b445'
-export const MAP_BOX_TOKEN = `pk.eyJ1IjoiY2hpcHNhMTIiLCJhIjoiY2wydTZoN2FrMDR0YjNqcGh2eTRjZTgyeCJ9.e--CfxdhU96ymIk9PEjtLQ`
-export const DEFAULT_AVATAR_URL = 'https://firebasestorage.googleapis.com/v0/b/mtwme-a1870.appspot.com/o/images%2FdefaultAvatar.png?alt=media&token=a2940a5b-ab7f-4c77-a7f9-86255106266e'
